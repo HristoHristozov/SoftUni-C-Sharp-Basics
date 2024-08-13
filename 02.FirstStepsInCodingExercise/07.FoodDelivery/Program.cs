8f3c9e7af0f93c75d0cff4c3d896eecd4eca05cc
Hristo Hristozov <hristozov95@gmail.com>
2024-08-13 11:32:36 +0300

    Commit 2nd Exercise

---
+++ 02.FirstStepsInCodingExercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 02.FirstStepsInCodingExercise/09.FishTank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09.FishTank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
